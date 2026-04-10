07337e618f7f63d6da9ef0e896aa177e0ba5beab
indahhaa <dianindahsafitri@students.amikom.ac.id>
2026-04-10 10:13:18 +0700

    Update: Tugas 1

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Profil</title>
+     <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class ="bg-slate-100 flex item-center justify-center h-screen">
+    <div class="bg-white p-8 rounded-xl shadow-lg border border-state-200 text-center max-w-sm w-full">
+    <h1 class="text-2xl font-blod text-slate-800 mb-2">Profil Kami</h1>
+    <p class="text-slate-500 mb-6">Event Amikom</p>
+    <a href="/" class="inline-block bg-indigo-600 text-white font-semibold py-2 px-6 rounded-lg hover:indigo-700 hover:shadow-md transition duration-300">Kembali ke Home</a>
+</body>
+</html>
