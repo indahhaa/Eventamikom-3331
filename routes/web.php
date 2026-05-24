a457a7678ca944c485c2dd790011feebab443cb9
indahhaa <dianindahsafitri@students.amikom.ac.id>
2026-05-24 17:06:55 +0700

    Eventamikom-3331

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,65 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EventController as EventAdminController;
 
 Route::get('/', function () {
-    return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
+    return view('welcome');
 });
 
-Route::get('/kontak', function () {
-    return view('contact');
+// Route::get('/profil', function () {
+//     return view('profil');
+// });
+
+// Route::get('/katalog', function () {
+//     return view('katalog');
+// });
+
+// Route::get('/bantuan', function () {
+//     return view('bantuan');
+// });
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
 });
 
-Route::get('/profil', function (){
-    return view('profil');
+Route::get('/event', function () {
+    return view('admin.event');
 });
 
-Route::get('/katalog', function (){
-    return view('katalog');
+Route::get('/transaksi', function () {
+    return view('admin.transaksi');
 });
 
-Route::get('/bantuan', function (){
-    return view('bantuan');
+Route::get('/detail-event', function () {
+    return view('event-detail');
 });
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ticket', function () {
+    return view('ticket');
+});
+
+
+// Route::group ( [ 'prefix'  =>  'admin' ,  'as'  =>  'admin.' ],  function  () {
+//     Route::get( '/' , [DashboardController:: class ,  'index' ])->name( 'index' );
+// });
 
 Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get( '/' , [DashboardController:: class ,  'index' ])->name( 'index' );
     Route::resource('events', EventAdminController::class);
 });
-
