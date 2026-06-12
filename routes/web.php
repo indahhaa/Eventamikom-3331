01a4487fe0b5614e3f95ebea1f1676f7e31eaed7
indahhaa <dianindahsafitri@students.amikom.ac.id>
2026-06-12 16:35:57 +0700

    Tugas 10
    Pertemuan 8

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Admin\EventController as EventAdminController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PartnerController;
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\Admin\TransactionController;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -31,6 +33,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('login', [AuthController::class, 'login'])->name('login.post');
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('transactions', [TransactionController::class, 'index'])->name('transactions.index');
 
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
@@ -40,4 +43,11 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::resource('partners', PartnerController::class);
 
 });
+
+Route::get('/checkout/{event}', [CheckoutController::class, 'create'])
+    ->name('checkout.create');
+
+Route::post('/checkout/{event}', [CheckoutController::class, 'store'])
+    ->name('checkout.store');
+
 });
